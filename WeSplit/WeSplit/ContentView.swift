3c4cd3ba838dbf9917277b6622efec444e63b86e
Larry N <larryn35@gmail.com>
2020-09-07 18:31:58 -0700

    dismiss keyboard when tap outside

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -68,3 +68,34 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+// Dismiss keyboard when tapped outside
+
+class AnyGestureRecognizer: UIGestureRecognizer {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        if let touchedView = touches.first?.view, touchedView is UIControl {
+            state = .cancelled
+
+        } else if let touchedView = touches.first?.view as? UITextView, touchedView.isEditable {
+            state = .cancelled
+
+        } else {
+            state = .began
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+       state = .ended
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+        state = .cancelled
+    }
+}
+
+extension SceneDelegate: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
